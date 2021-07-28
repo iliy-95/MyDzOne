9a6377df931b96fd87974ff77b8f2532d1bef253
iliy <gromov218974@mail.ru>
2021-07-28 21:12:34 +0400

    Первая задача. TAsk3
    Вторая задачаю.

---
+++ app/src/main/java/com/example/mydzone/MainActivityCalendar.java
@@ -0,0 +1,14 @@
+package com.example.mydzone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityCalendar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_calendar);
+    }
+}
